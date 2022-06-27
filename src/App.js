69a904bcc9546dcf5d20a7a908505ed01b73f865
procode47 <iloenyenwavictor@gmail.com>
2022-06-27 22:19:59 +0100

    feat: added dark mode toggle switch and adjusted padding in platform-security component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import "./styles/app.scss";
 import  HomeTabs  from "./components/tabs";
-import { AiOutlineMore } from "react-icons/ai";
-import {FiLock} from "react-icons/fi";
-
+import { FiSun,FiMoon } from "react-icons/fi";
+import { FiLock } from "react-icons/fi";
+import { useState } from "react";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const handleToggle = () => {
+    setDarkMode(!darkMode)
+  }
  
   return (
     <>
       <header>
         <h1><FiLock style={{marginRight:10}}/>Web Secure</h1>
-        <AiOutlineMore style={{cursor:"pointer"}} size={20}/>
+        <div className="mode-toggle">
+          {darkMode ? <FiSun size={20} onClick={handleToggle} />
+          : <FiMoon size={20} onClick={handleToggle} />}
+        </div>
       </header>
       <hr />
       <HomeTabs />
